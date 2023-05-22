78e968670314d6f084f5b5e5fb81dd2b2ff7446b
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2023-05-21 17:02:50 -0700

    Feat: Generate Grid

---
+++ untitled-circuit-game/Assets/Scripts/GridManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private Object GridCellPrefab;
+    [SerializeField] private Material GridMat1;
+    [SerializeField] private Material GridMat2;
+    [SerializeField] private int width = 11;
+    [SerializeField] private int height = 11;
+
+    private void Start()
+    {
+        const int cellWidth = 1;
+        const int cellHeight = 1;
+        Vector3 startPos = this.gameObject.transform.position - new Vector3((width * cellWidth) / 2, 0, (height * cellHeight) / 2);
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Vector3 spawnPos = startPos + new Vector3(j * cellWidth, 0, i * cellHeight);
+                var newObj = GameObject.Instantiate(GridCellPrefab, spawnPos, Quaternion.identity) as GameObject;
+                var newObjMR = newObj.GetComponent<MeshRenderer>();
+                if ((i + j) % 2 == 0)
+                {
+                    newObjMR.material = GridMat1;
+                }
+                else
+                {
+                    newObjMR.material = GridMat2;
+                }
+            }
+        }
+    }
+}
