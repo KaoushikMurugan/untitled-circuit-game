9d0e4d6163586de53cb61fa262472e8455a7730c
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2023-05-21 17:13:16 -0700

    Feat: Store the created grid cells in an array

--- untitled-circuit-game/Assets/Scripts/GridManager.cs
+++ untitled-circuit-game/Assets/Scripts/GridManager.cs
@@ -7,9 +7,11 @@ public class GridManager : MonoBehaviour
     [SerializeField] private Material GridMat2;
     [SerializeField] private int width = 11;
     [SerializeField] private int height = 11;
-
+    private GameObject[,] GridCells;
     private void Start()
     {
+        GridCells = new GameObject[height, width];
+
         const int cellWidth = 1;
         const int cellHeight = 1;
         Vector3 startPos = this.gameObject.transform.position - new Vector3((width * cellWidth) / 2, 0, (height * cellHeight) / 2);
@@ -19,6 +21,7 @@ public class GridManager : MonoBehaviour
             {
                 Vector3 spawnPos = startPos + new Vector3(j * cellWidth, 0, i * cellHeight);
                 var newObj = GameObject.Instantiate(GridCellPrefab, spawnPos, Quaternion.identity) as GameObject;
+                GridCells[i,j] = newObj;
                 var newObjMR = newObj.GetComponent<MeshRenderer>();
                 if ((i + j) % 2 == 0)
                 {
@@ -30,5 +33,10 @@ public class GridManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    public GameObject[,] getGridCells()
+    {
+        return GridCells;
     }
 }
